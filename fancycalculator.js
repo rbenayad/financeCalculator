eaab6e0f96158004f38320a4857c2c1228f7f968
Rami Benayad-Cherif <RamiBenayadCherif@1x-vl907-128-119-24-184.wireless.umass.edu>
2015-01-29 15:14:31 -0500

    comments

--- fancycalculator.js
+++ fancycalculator.js
@@ -1,30 +1,30 @@
 var financeOption = "PV"
-var financeCallbacks = {
-	ir : function(){
-		var pval = parseFloat($("#pval").val());
-		var fval = parseFloat($("#fval").val());
+var financeCallbacks = { //start ofthe financeCallbacks object
+	ir : function(){ //start of the ir key
+		var pval = parseFloat($("#pval").val()); //parseFloat changes a string to a number
+		var fval = parseFloat($("#fval").val()); //dollar sign calls jquery
 		var nyears = parseFloat($("#nyears").val());
-		var res = ((Math.pow((fval / pval), (1 / nyears)) - 1) * 100).toFixed(4) 
+		var res = ((Math.pow((fval / pval), (1 / nyears)) - 1) * 100).toFixed(4) //equation for interest rate
 		console.log(res);
 		$("#answer").text(res + "%");
 	},
-	rorn: function(){
+	rorn: function(){ //start of the 'rorn' key
 		var pval = parseFloat($("#pval").val());
 		var fval = parseFloat($("#fval").val());
 		var rate = parseFloat($("#rate").val());
-		var res = ((Math.log(fval / pval)) / (Math.log(1 + rate))).toFixed(3) 
+		var res = ((Math.log(fval / pval)) / (Math.log(1 + rate))).toFixed(3) //equation for interest rate
 		console.log(res);
 		$("#answer").text(res + " years");
 	}
 
 }
 
-$("document").ready(function(){
+$("document").ready(function(){ //when DOM is fully loaded
 	$(".lora,.PVoFV,.param").hide();
 	$(".basic").show();
-	$("#financeType").change(function(e){
+	$("#financeType").change(function(e){ //e can be any variable name
 		var v = $("#financeType").val();
-		if(v === "PV" || v === "FV"){
+		if(v === "PV" || v === "FV"){ //
 			$(".lora").show();
 			$(".PVoFV").hide();
 		}
