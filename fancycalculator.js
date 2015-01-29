b7406161c538ec255f5bd0c21769e6c5c7759b2c
sachour <sachour@mit.edu>
2015-01-29 00:06:35 -0500

    added answer box

--- fancycalculator.js
+++ fancycalculator.js
@@ -1,3 +1,16 @@
+var financeOption = "PV"
+var financeCallbacks = {
+	ir : function(){
+		var pval = parseFloat($("#pval").val());
+		var fval = parseFloat($("#fval").val());
+		var nyears = parseFloat($("#nyears").val());
+		var res = ((Math.pow((fval / pval), (1 / nyears)) - 1) * 100).toFixed(4) 
+		console.log(res);
+		$("#answer").text(res);
+	}
+
+}
+
 $("document").ready(function(){
 	$(".lora,.PVoFV,.param").hide();
 	$(".basic").show();
@@ -14,9 +27,17 @@ $("document").ready(function(){
 				$(".PVoFV").show();
 			}
 		}
-
+		financeOption = v;
 		if(v == "ir"){
+			$(".param").hide();
 			$("#pval,#fval,#nyears").show();
 		}
+		else if(v == "rorn"){
+			$(".param").hide();
+			$("#pval,#fval,#rate").show();
+		}
+	})
+	$("#pval,#fval,#nyears").change(function(e){
+		financeCallbacks[financeOption]();
 	})
 })
